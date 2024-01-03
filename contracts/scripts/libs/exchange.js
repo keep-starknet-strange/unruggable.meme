06f86265f00bfcc6cbc755cfe6779d749ac8ffa7
bal7hazar <bal7hazar@proton.me>
2024-01-03 16:27:34 +0100

    🐛 Update JediSwap contract addresses

--- contracts/scripts/libs/exchange.js
+++ contracts/scripts/libs/exchange.js
@@ -2,7 +2,8 @@ const EXCHANGES = {
   mainnet: [
     {
       name: "JediSwap",
-      contract_address: "",
+      contract_address:
+        "0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023",
     },
   ],
   goerli: [
@@ -15,8 +16,7 @@ const EXCHANGES = {
   sepolia: [
     {
       name: "JediSwap",
-      contract_address:
-        "0x02bcc885342ebbcbcd170ae6cafa8a4bed22bb993479f49806e72d96af94c965",
+      contract_address: "",
     },
   ],
 };
