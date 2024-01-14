403a5c3ee8d0186db8748ca9fe294c10c9e127b4
Charles Lanier <clanier.dev@gmail.com>
2024-01-14 15:58:54 +0100

    Add responsive modal (#143)
    just made modal reponsive

--- frontend/src/components/Modal/Content.css.ts
+++ frontend/src/components/Modal/Content.css.ts
@@ -1,24 +1,41 @@
 import { style } from '@vanilla-extract/css'
-import { sprinkles } from 'src/theme/css/sprinkles.css'
+import { breakpoints, sprinkles } from 'src/theme/css/sprinkles.css'
 
 export const content = style([
+  {
+    '@media': {
+      [`(min-width: ${breakpoints.sm}px)`]: {
+        padding: '32px',
+        top: '50%',
+        left: '50%',
+        width: '480px',
+        transform: 'translate(-50%, -50%)',
+        bottom: 'unset',
+      },
+    },
+  },
   sprinkles({
     borderWidth: '1px',
     borderStyle: 'solid',
     borderColor: 'border1',
     borderRadius: '10',
-    padding: '24',
     background: 'bg1',
     zIndex: 'modal',
     position: 'fixed',
-    left: '1/2',
-    top: '1/2',
-    width: '480',
+    bottom: '0',
+    top: '0',
+    paddingX: '32',
+    paddingY: '88',
+    width: 'full',
   }),
-  {
-    transform: 'translate(-50%, -50%)',
-  },
 ])
+
+export const title = sprinkles({
+  fontSize: {
+    sm: '32',
+    md: '24',
+  },
+})
 
 export const closeContainer = sprinkles({
   color: 'text1',
