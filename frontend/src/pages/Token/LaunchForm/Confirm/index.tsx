3f6954a15ff2d3dfc8cf0ab4c345a1281ba79541
0xChqrles <clanier.dev@gmail.com>
2024-02-18 15:40:32 -0700

    fix conflicts

--- frontend/src/pages/Token/LaunchForm/Confirm/index.tsx
+++ frontend/src/pages/Token/LaunchForm/Confirm/index.tsx
@@ -21,9 +21,6 @@ export default function ConfirmForm({ previous }: LastFormPageProps) {
     }
   }, [amm, previous])
 
-  // team allocation
-  const teamAllocationTotalPercentage = useTeamAllocationTotalPercentage(memecoinInfos.totalSupply)
-
   return (
     <Column gap="42">
       <Text.Custom color="text2" fontWeight="normal" fontSize="24">
