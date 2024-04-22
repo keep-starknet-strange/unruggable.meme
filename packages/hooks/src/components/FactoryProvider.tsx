6ed808b4f805634e8d826e87921907a3481fb8fd
Uğur Eren <contact@ugureren.net>
2024-04-22 09:37:31 +0300

    Create a default factory if not provided

--- packages/hooks/src/components/FactoryProvider.tsx
+++ packages/hooks/src/components/FactoryProvider.tsx
@@ -1,12 +1,35 @@
+import { starknetChainId, useNetwork, useProvider } from '@starknet-react/core'
 import { Factory } from 'core'
+import { useEffect, useRef } from 'react'
+import { ProviderInterface } from 'starknet'
 
 import { FactoryContext } from '../contexts/Factory'
 
 export type FactoryProviderProps = {
-  factory: Factory
+  factory?: Factory
+  provider?: ProviderInterface
   children?: React.ReactNode
 }
 
-export const FactoryProvider = ({ factory, children }: FactoryProviderProps) => {
-  return <FactoryContext.Provider value={factory}>{children}</FactoryContext.Provider>
+export const FactoryProvider = ({ factory, provider, children }: FactoryProviderProps) => {
+  const defaultFactory = useRef<Factory | undefined>()
+
+  const { provider: defaultProvider } = useProvider()
+  const { chain } = useNetwork()
+
+  useEffect(() => {
+    // If a factory is provided, use it instead of creating a new one.
+    if (factory) return
+
+    const chainId = chain.id ? starknetChainId(chain.id) : undefined
+    if (!chainId || !(provider ?? defaultProvider)) return
+
+    defaultFactory.current = new Factory({ provider: provider ?? defaultProvider, chainId })
+  }, [factory, provider, defaultProvider, chain.id])
+
+  const factoryToUse = factory ?? defaultFactory.current
+
+  if (!factoryToUse) return children
+
+  return <FactoryContext.Provider value={factoryToUse}>{children}</FactoryContext.Provider>
 }
--- packages/hooks/src/components/Provider.tsx
+++ packages/hooks/src/components/Provider.tsx
@@ -1,12 +1,25 @@
 import { QueryClient } from '@tanstack/react-query'
 import { Factory } from 'core'
+import { ProviderInterface } from 'starknet'
 
 import { FactoryProvider } from './FactoryProvider'
 import { QueryProvider } from './QueryProvider'
 
 export type ProviderProps = {
-  /** Factory client to use in the hooks. */
-  factory: Factory
+  /**
+   * Factory client to use in the hooks.
+   * If not provided, the hooks will use the default factory client.
+   * However, it is highly recommended to provide a factory client.
+   * If the factory client is provided, the provider interface will be ignored.
+   */
+  factory?: Factory
+
+  /**
+   * Provider Interface to use if the factory client is not provided.
+   * If not provided, the hooks will use the default provider interface.
+   * If factory client is provided, this prop will be ignored.
+   */
+  provider?: ProviderInterface
 
   /** React-query client to use. */
   queryClient?: QueryClient
