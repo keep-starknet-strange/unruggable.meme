7e4ddda322c1ea476ea8f4280b0ceb20cc8e3fde
Uğur Eren <contact@ugureren.net>
2024-04-30 22:41:37 +0300

    SDK JSDoc comments

--- packages/core/src/factory/interface.ts
+++ packages/core/src/factory/interface.ts
@@ -19,27 +19,71 @@ export type FactoryConfig = {
 export abstract class FactoryInterface {
   public abstract config: FactoryConfig
 
+  /**
+   * Get a memecoin by address. Returns the results of both `getBaseMemecoin` and `getMemecoinLaunchData`.
+   * @param address Memecoin address
+   */
   public abstract getMemecoin(address: string): Promise<Memecoin | undefined>
 
+  /**
+   * Get a memecoin's base details by address. This includes the memecoin's name, symbol, owner, decimals, and total supply.
+   * @param address Memecoin address
+   */
   public abstract getBaseMemecoin(address: string): Promise<BaseMemecoin | undefined>
 
+  /**
+   * Get a memecoin's launch details by address. This includes the memecoin's quote token, team allocations, and liquidity.
+   * @param address Memecoin address
+   */
   public abstract getMemecoinLaunchData(address: string): Promise<LaunchedMemecoin>
 
+  /**
+   * Get the starting market cap of a memecoin at launch.
+   * @param memecoin Result of `getMemecoin`
+   * @param quoteTokenPriceAtLaunch Quote token price at launch as a Fraction
+   */
   public abstract getStartingMarketCap(memecoin: Memecoin, quoteTokenPriceAtLaunch?: Fraction): Fraction | undefined
 
+  /**
+   * Get the collectable ekubo fees of a memecoin.
+   * @param memecoin Result of `getMemecoin`
+   */
   public abstract getEkuboFees(memecoin: Memecoin): Promise<Fraction | undefined>
 
+  /**
+   * Get the calldata to collect ekubo fees of a memecoin.
+   * @param memecoin Result of `getMemecoin`
+   */
   public abstract getCollectEkuboFeesCalldata(memecoin: Memecoin): { calls: CallDetails[] } | undefined
 
+  /**
+   * Get the calldata to extend the liquidity lock of a memecoin.
+   * @param memecoin Result of `getMemecoin`
+   * @param seconds Amount of time to extend the liquidity lock in seconds
+   */
   public abstract getExtendLiquidityLockCalldata(
     memecoin: Memecoin,
     seconds: number,
   ): { calls: CallDetails[] } | undefined
 
+  /**
+   * Get the calldata to deploy a memecoin.
+   * @param data Data to deploy a memecoin
+   */
   public abstract getDeployCalldata(data: DeployData): { tokenAddress: string; calls: CallDetails[] }
 
+  /**
+   * Get the calldata to launch a deployed memecoin on Ekubo.
+   * @param memecoin Result of `getMemecoin`
+   * @param data Data to launch a memecoin
+   */
   public abstract getEkuboLaunchCalldata(memecoin: Memecoin, data: EkuboLaunchData): Promise<{ calls: CallDetails[] }>
 
+  /**
+   * Get the calldata to launch a deployed memecoin on a standard AMM.
+   * @param memecoin Result of `getMemecoin`
+   * @param data Data to launch a memecoin
+   */
   public abstract getStandardAMMLaunchCalldata(
     memecoin: Memecoin,
     data: StandardAMMLaunchData,
