cda6e3b643abd2eeb0c6b2b0f3e770758d9488de
Rhumsteack <144366163+Rhumsteak@users.noreply.github.com>
2024-05-10 19:48:12 +0200

    Add files via upload

---
+++ infra/indexers/src/unruggableMemecoin-launch.indexer.ts
@@ -0,0 +1,57 @@
+import { Block, hash } from "./deps.ts";
+import { hexToAscii } from "./utils.ts";
+import { FACTORY_ADDRESS, STARTING_BLOCK } from "./unruggableMemecoin.ts";
+
+const filter = {
+  header: {
+    weak: true,
+  },
+  events: [
+    {
+      fromAddress: FACTORY_ADDRESS,
+      keys: [hash.getSelectorFromName("MemecoinLaunched") as FieldElement],
+      includeReceipt: false,
+    },
+  ],
+};
+
+export const config = {
+  streamUrl: "https://mainnet.starknet.a5a.ch",
+  startingBlock: STARTING_BLOCK,        
+  network: "starknet",
+  finality: "DATA_STATUS_ACCEPTED",
+  filter,
+  sinkType: "postgres",
+  sinkOptions: {
+    connectionString: "",
+    tableName: "unrugmeme_launch",
+  },
+};
+
+
+export default function DecodeUnruggableMemecoinLaunch({ header, events }: Block) {
+  const { blockNumber, blockHash, timestamp } = header!;
+  
+  return (events ?? []).map(({ event, transaction }) => {
+    const transactionHash = transaction.meta.hash;
+    const eventId = `${transactionHash}_${event.index ?? 0}`;
+
+    const [memecoin_address, quote_token, exchange_name] =
+      event.data;
+    
+    const exchange_name_decoded = hexToAscii(exchange_name);
+
+    return {
+      network: "starknet-mainnet",
+      block_hash: blockHash,
+      block_number: +blockNumber,
+      block_timestamp: timestamp,
+      transaction_hash: transactionHash,
+      event_id: eventId,
+      memecoin_address: memecoin_address,
+      quote_token: quote_token,
+      exchange_name: exchange_name_decoded,
+      created_at: new Date().toISOString(),
+    };
+  });
+}
---
+++ infra/indexers/src/unruggableMemecoin-transfers.indexer.ts
@@ -0,0 +1,70 @@
+import { Block, hash } from "./deps.ts";
+import { FACTORY_ADDRESS, STARTING_BLOCK } from "./unruggableMemecoin.ts";
+
+export const config = {
+  filter: {
+    header: { weak: true },
+    events: [
+      {
+        fromAddress: FACTORY_ADDRESS,
+        keys: [hash.getSelectorFromName("MemecoinLaunched") as FieldElement],
+        includeReceipt: false,
+      },
+    ],
+  },
+  streamUrl: "https://mainnet.starknet.a5a.ch",
+  startingBlock: STARTING_BLOCK,        
+  network: "starknet",
+  finality: "DATA_STATUS_ACCEPTED",
+  sinkType: "postgres",
+  sinkOptions: {
+    connectionString: "",
+    tableName: "unrugmeme_transfers",
+  },
+};
+
+export function factory({ header, events }) {
+  const launchEvents = (events ?? []).map(({ event }) => { 
+    const memecoin_address = event.data[0];
+    return {
+      fromAddress: memecoin_address,
+      keys: [hash.getSelectorFromName("Transfer") as FieldElement],
+      includeReceipt: false,
+    };
+  });
+  
+  return {
+    filter: {
+      header: { weak: true },
+      events: launchEvents,
+    },
+  };
+}
+
+
+export default function DecodeUnruggableMemecoinLaunch({ header, events }: Block) {
+  const { blockNumber, blockHash, timestamp } = header!;
+  
+  return (events ?? []).map(({ event, transaction }) => {
+    const transactionHash = transaction.meta.hash;
+    const transferId = `${transactionHash}_${event.index ?? 0}`;
+    const sender = event.keys[1];
+    const recipient = event.keys[2];
+    const amount = BigInt(event.data[0]);
+    const memecoin_address = event.fromAddress;
+    
+    return {
+      network: "starknet-mainnet",
+      block_hash: blockHash,
+      block_number: +blockNumber,
+      block_timestamp: timestamp,
+      transaction_hash: transactionHash,
+      transfer_id: transferId,
+      sender: sender,
+      recipient: recipient,
+      memecoin_address: memecoin_address,
+      amount: amount.toString(10),
+      created_at: new Date().toISOString(),
+    };
+  });
+}
