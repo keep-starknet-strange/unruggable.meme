18c0f2a765e68b19b8830bba6b2e7e2efca56eff
remiroyc <remiroycourt@gmail.com>
2024-11-19 23:15:47 +0100

    feat: wip

--- packages/contracts/scripts/deploy.js
+++ packages/contracts/scripts/deploy.js
@@ -1,22 +1,22 @@
-import colors from "colors";
-import { deployFactory, deployTokenLocker } from "./libs/contract.js";
+/* import colors from 'colors' */
+import { deployFactory, deployLockManager } from './libs/contract.js'
 
-const MIN_LOCK_TIME = 15_721_200; // 6 months
+const MIN_LOCK_TIME = 15_721_200 // 6 months
 
 const main = async () => {
-  console.log(`   ____          _         `.red);
-  console.log(`  |    \\ ___ ___| |___ _ _ `.red);
-  console.log(`  |  |  | -_| . | | . | | |`.red);
-  console.log(`  |____/|___|  _|_|___|_  |`.red);
-  console.log(`            |_|       |___|`.red);
+  console.log(`   ____          _         `.red)
+  console.log(`  |    \\ ___ ___| |___ _ _ `.red)
+  console.log(`  |  |  | -_| . | | . | | |`.red)
+  console.log(`  |____/|___|  _|_|___|_  |`.red)
+  console.log(`            |_|       |___|`.red)
 
   // Token Locker
-  console.log(`\n${"Deploying TokenLocker contract".blue}`);
-  await deployTokenLocker(MIN_LOCK_TIME);
+  console.log(`\n${'Deploying LockManager contract'.blue}`)
+  await deployLockManager(MIN_LOCK_TIME)
 
   // Factory
-  console.log(`\n${"Deploying Factory contract".blue}`);
-  await deployFactory();
-};
+  /*   console.log(`\n${'Deploying Factory contract'.blue}`)
+  await deployFactory() */
+}
 
-main();
+main()
--- packages/contracts/scripts/libs/exchange.js
+++ packages/contracts/scripts/libs/exchange.js
@@ -1,29 +1,21 @@
 const EXCHANGES = {
   mainnet: [
     {
-      name: "JediSwap",
-      contract_address:
-        "0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023",
-    },
-  ],
-  goerli: [
-    {
-      name: "JediSwap",
-      contract_address:
-        "0x02bcc885342ebbcbcd170ae6cafa8a4bed22bb993479f49806e72d96af94c965",
+      name: 'JediSwap',
+      contract_address: '0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023',
     },
   ],
   sepolia: [
     {
-      name: "JediSwap",
-      contract_address: "",
+      name: 'JediSwap',
+      contract_address: '',
     },
   ],
-};
+}
 
 export const getExchanges = (network) => {
   if (!EXCHANGES[network.toLowerCase()]) {
-    throw new Error(`Network ${network} not found`);
+    throw new Error(`Network ${network} not found`)
   }
-  return EXCHANGES[network.toLowerCase()];
-};
+  return EXCHANGES[network.toLowerCase()]
+}
